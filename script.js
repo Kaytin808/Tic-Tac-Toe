857cf4b19dc7f68eec210cb5d384c4573dd205af
Justin <kaytin143@gmail.com>
2021-12-16 01:17:28 -0700

    added factory function for playerOne

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+
 var cell = document.querySelectorAll('.cell')
 
+// Input player 1 'x' //
 cell.forEach((button) => {
     button.addEventListener('click', () => {
         button.innerHTML = 'x'
     })
 })
+
+const playerOne = (myName, age) => {
+    const sayHello = () => alert('Hello ' + myName + '!');
+    return { myName, age, sayHello };
+}
+
+const justin = playerOne('Justin', 26);
