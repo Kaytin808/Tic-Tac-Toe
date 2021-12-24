762958a6a54a8e97a8d10c1e6fcfac7386c5859b
Justin <kaytin143@gmail.com>
2021-12-24 03:26:45 -0700

    Added function to check for win

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
+var cell1 = document.querySelector('.cell1')
+var cell2 = document.querySelector('.cell2')
+var cell3 = document.querySelector('.cell3')
+var cell4 = document.querySelector('.cell4')
+var cell5 = document.querySelector('.cell5')
+var cell6 = document.querySelector('.cell6')
+var cell7 = document.querySelector('.cell7')
+var cell8 = document.querySelector('.cell8')
+var cell9 = document.querySelector('.cell9')
 
-var cell = document.querySelectorAll('.cell')
 
-// Input player 1 'x' //
-cell.forEach((button) => {
-    button.addEventListener('click', () => {
-        button.innerHTML = 'x'
-    })
+
+var gameBoard = [];
+
+
+
+
+function playGame() {
+cell1.addEventListener('click', () => {
+    cell1.innerHTML = 'x';
+    gameBoard.push('x')
+    checkWin()
 })
-
-const playerOne = (myName, age) => {
-    const sayHello = () => alert('Hello ' + myName + '!');
-    return { myName, age, sayHello };
+cell2.addEventListener('click', () => {
+    cell2.innerHTML = 'x';
+    gameBoard.push('x')
+    checkWin()
+})
+cell3.addEventListener('click', () => {
+    cell3.innerHTML = 'x';
+    gameBoard.push('x')
+    checkWin()
+})
+cell4.addEventListener('click', () => {
+    cell4.innerHTML = 'x';
+    gameBoard.push('x')
+})
 }
 
-const justin = playerOne('Justin', 26);
+function checkWin() {
+    if (cell1.innerHTML == 'x' && cell2.innerHTML == 'x' && cell3.innerHTML == 'x') {
+        alert('you win!')
+        cell1.innerHTML = ''
+        cell2.innerHTML = ''
+        cell3.innerHTML = ''
+    }
+}
+playGame();
+// const playerOne = (myName, age) => {
+//     const sayHello = () => alert('Hello ' + myName + '!');
+//     return { myName, age, sayHello };
+// }
+
+// const justin = playerOne('Justin', 26);
