9b5c1010109b445dc37b1f45a4144317d46f4c85
Justin Moore <kaytin143@gmail.com>
2022-01-14 18:10:33 -0700

    added turns for players

--- script.js
+++ script.js
@@ -1,51 +1,27 @@
-var gameBoard = [
-    {
-        box1:''
+const gameBoard = [];
+const cell = document.querySelectorAll('.cell')
+const firstPlayer = 'o'
+const secondPlayer = 'x'
+
+let currentPlayer;
+let turn = true;
+
+function handleClick(e) {
+    const id = e.target.id;
+    if (!gameBoard[id]) {
+        gameBoard[id] = currentPlayer;
+        if (turn === true) {
+            currentPlayer = firstPlayer;
+            e.target.innerText = currentPlayer; 
+        } else if (turn === false) {
+            currentPlayer = secondPlayer;
+            e.target.innerText = currentPlayer; 
+        }
+        turn = !turn;
     }
-]
+}
 
 
-var cell = document.querySelectorAll('.cell')
-
-
-
-
-player1();
-
-function player1 () {
     cell.forEach((box1) =>{
-        box1.addEventListener('click', () => {
-            box1.innerHTML = 'x'
-
-            player2();
+        box1.addEventListener('click', handleClick)
     })
-})
-}
-
-function player2() {
-cell.forEach((box2) => {
-    box2.addEventListener('click', () => {
-        box2.innerHTML = 'o'
-
-        player1();
-    })
-})
-}
-
-
-
-
-
-
-
-
-
-
-function checkWin() {
-    if (cell1.innerHTML == 'x' && cell2.innerHTML == 'x' && cell3.innerHTML == 'x') {
-        alert('you win!')
-        cell1.innerHTML = ''
-        cell2.innerHTML = ''
-        cell3.innerHTML = ''
-    }
-}
