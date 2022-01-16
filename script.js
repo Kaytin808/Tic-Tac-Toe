45bb71b6bee9ddba24eaeeaa8a461893d84c003a
Justin Moore <kaytin143@gmail.com>
2022-01-15 19:41:24 -0700

    Fixed array for gameboard

--- script.js
+++ script.js
@@ -9,19 +9,29 @@ let turn = true;
 function handleClick(e) {
     const id = e.target.id;
     if (!gameBoard[id]) {
-        gameBoard[id] = currentPlayer;
+        gameBoard[id] = firstPlayer;
         if (turn === true) {
             currentPlayer = firstPlayer;
-            e.target.innerText = currentPlayer; 
+            e.target.innerText = currentPlayer;
+            
         } else if (turn === false) {
             currentPlayer = secondPlayer;
+            gameBoard[id] = secondPlayer
             e.target.innerText = currentPlayer; 
+            
         }
         turn = !turn;
+        console.log(gameBoard)
     }
 }
 
-
-    cell.forEach((box1) =>{
+// When cells are clicked run this function //
+    cell.forEach((box1) => {
         box1.addEventListener('click', handleClick)
     })
+
+function checkWinner() {
+    if (gameBoard[2] === firstPlayer) {
+        alert('its same')
+    }
+}
